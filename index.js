2e39fe0240ea0ee95c5c60d1e643c26a1983a822
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-12-27 12:41:30 +0600

    Created a API named /buyerProduct

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ async function run() {
     const categoryCollection = client.db("oldenGoodsDB").collection("category");
     const reviewsCollection = client.db("oldenGoodsDB").collection("userReview");
     const usersCollection = client.db("oldenGoodsDB").collection("users");
+    const buyerProductsCollection = client.db("oldenGoodsDB").collection("buyerProducts");
 
 
 
@@ -56,22 +57,49 @@ async function run() {
 
 
     // Products related API
-    app.get('/products',async(req,res)=>{
-        const result = await productsCollection.find().toArray();
-        res.send(result)
-    });
+    app.post('/products',async(req,res)=>{
+      const product = req.body;
+      const result =await productsCollection.insertOne(product);
+      res.send(result);
+  });
+  app.get('/products',async(req,res)=>{
+    const filter = req.query
+    const options ={
+        sort :{
+            date : 1
+        }
+    };
+    const cursor = productsCollection.find(filter,options);
+    const result =await cursor.toArray();
+    res.send(result);
+});
     app.get('/products/:id', async(req,res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await productsCollection.findOne(query);
       res.send(result);
   });
+   app.get('/products', async (req,res)=>{
+     let query = {}
+     if(req.query?.email){
+        query = {email : req.query.email}
+     }
+     const result = await productsCollection.find(query).toArray();
+     res.send(result);
+  });
+  app.delete('/products/:id', async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await productsCollection.deleteOne(query);
+    res.send(result);
+});
+
 
 
 
   // USER relate API
     app.post('/users',async(req,res)=>{
-      const user = req.body;
+    const user = req.body;
     const query ={email: user.email}
     const existingUser = await usersCollection.findOne(query);
     if(existingUser){
@@ -80,6 +108,35 @@ async function run() {
     const result = await usersCollection.insertOne(user);
     res.send(result);
   });
+
+
+
+
+    
+  // buyerProducts related api or CART relsted
+      app.post('/buyerProduct',async(req,res)=>{
+      const cartItem = req.body;
+      const result = await buyerProductsCollection.insertOne(cartItem);
+       res.send(result);
+   });
+      app.get('/buyerProduct',async(req,res)=>{
+      let query = {}
+      if(req.query?.email){
+      query = {email : req.query.email}
+    }
+      const result = await buyerProductsCollection.find(query).toArray();
+      res.send(result);
+   });
+   app.delete('/buyerProduct/:id',async(req,res)=>{
+     const id = req.params.id;
+     const query = {_id : new ObjectId(id)}
+     const result = await buyerProductsCollection.deleteOne(query);
+     res.send(result)
+   });
+
+
+
+
 
 
 
